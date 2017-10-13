3b1c25e00f9cb520019df6dfd0f3c75ad1298ded
Jon Smajda <jon@smajda.com>
2017-10-13 16:00:06 -0500

    Add FileHandler

--- validate_people.py
+++ validate_people.py
@@ -2,6 +2,8 @@ import argparse
 import logging
 import sys
 
+from os.path import abspath, dirname, join
+
 from people.tasks import validate_csv
 
 logger = logging.getLogger()
@@ -11,6 +13,11 @@ logger.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler(stream=sys.stdout)
 stream_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
+
+# and a FileHandler
+file_handler = logging.FileHandler(filename=join(abspath(dirname(__name__)), 'validate_people.log'))
+file_handler.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
 
 
 if __name__ == '__main__':
