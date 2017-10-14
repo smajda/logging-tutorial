3381dd5e42cd4b00f952f890fcab59d9cf0ca2e5
Jon Smajda <jon@smajda.com>
2017-10-13 23:29:59 -0500

    Configure stream handler

--- validate_people.py
+++ validate_people.py
@@ -1,6 +1,16 @@
 import argparse
+import logging
+import sys
 
 from people.tasks import validate_csv
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# let's add a custom StreamHandler
+stream_handler = logging.StreamHandler(stream=sys.stdout)
+stream_handler.setLevel(logging.DEBUG)
+logger.addHandler(stream_handler)
 
 
 if __name__ == '__main__':
